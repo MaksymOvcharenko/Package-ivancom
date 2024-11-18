eadd426d0a5d65d1e726139f955464350ca63973
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-11-18 15:36:03 +0100

    new

--- src/components/SenderAddress/SenderAddress.jsx
+++ src/components/SenderAddress/SenderAddress.jsx
@@ -45,7 +45,8 @@
 
 // export default SenderAddress;
 import { useForm } from "react-hook-form";
-import { useRef, useEffect, useState } from "react";
+import { InpostGeowidget } from "react-inpost-geowidget";
+import { useState } from "react";
 import styles from "./SenderAddress.module.css";
 
 const SenderAddress = ({ onNext, onPrev }) => {
@@ -55,28 +56,13 @@ const SenderAddress = ({ onNext, onPrev }) => {
     setValue, // Для встановлення значень у форму
     formState: { errors },
   } = useForm();
-  const geowidgetRef = useRef(null);
   const [selectedPoint, setSelectedPoint] = useState("");
 
-  useEffect(() => {
-    // Ініціалізація віджета
-    const initGeoWidget = () => {
-      if (!window.customElements.get("inpost-geowidget")) {
-        console.error("InPost геовіджет не завантажився.");
-        return;
-      }
-
-      geowidgetRef.current.addEventListener("point", (event) => {
-        const point = event.detail;
-        setSelectedPoint(point.name); // Зберігаємо вибраний поштомат у стані
-        setValue("senderAddress.postamat", point.name); // Задаємо значення у форму
-      });
-    };
-
-    if (geowidgetRef.current) {
-      initGeoWidget();
-    }
-  }, [setValue]);
+  const onPointCallback = (point) => {
+    console.log("Вибраний поштомат:", point);
+    setSelectedPoint(point.name); // Зберігаємо вибраний поштомат у стані
+    setValue("senderAddress.postamat", point.name); // Задаємо значення у форму
+  };
 
   const onSubmit = (data) => {
     onNext({ senderAddress: data });
@@ -87,13 +73,12 @@ const SenderAddress = ({ onNext, onPrev }) => {
       <div className={styles.field}>
         <label>Поштомат InPost</label>
         <div className={styles.geoWidgetContainer}>
-          <inpost-geowidget
-            ref={geowidgetRef}
-            id="inpost-widget"
+          <InpostGeowidget
             token="eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJzQlpXVzFNZzVlQnpDYU1XU3JvTlBjRWFveFpXcW9Ua2FuZVB3X291LWxvIn0.eyJleHAiOjIwNDcyOTk0MjAsImlhdCI6MTczMTkzOTQyMCwianRpIjoiMmQ5ZjY3MzMtMDhiMi00MDMwLTk5MGEtMTgyM2ViZTVhOWQ1IiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5pbnBvc3QucGwvYXV0aC9yZWFsbXMvZXh0ZXJuYWwiLCJzdWIiOiJmOjEyNDc1MDUxLTFjMDMtNGU1OS1iYTBjLTJiNDU2OTVlZjUzNTpnV05US05EV1pqRDBUZGxCNUNDS2NDeGVOSmRPRmFTRmhkSUM5ZG8zTHBJIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic2hpcHgiLCJzZXNzaW9uX3N0YXRlIjoiYjk0ODI5MjItM2U3OC00MjkwLTkxNTMtMzk4NGQ3NThjYzE2Iiwic2NvcGUiOiJvcGVuaWQgYXBpOmFwaXBvaW50cyIsInNpZCI6ImI5NDgyOTIyLTNlNzgtNDI5MC05MTUzLTM5ODRkNzU4Y2MxNiIsImFsbG93ZWRfcmVmZXJyZXJzIjoicGFja2FnZS1pdmFuY29tLnZlcmNlbC5hcHAiLCJ1dWlkIjoiMjYyMzAwODktZjJmNC00MjFlLWE2MDctNGJhZDJjNDk3NmIyIn0.ZUC2HVgxkD60q76gWT0-pFmDdEfOO7m7v62qQBoDoPUm7WklX7-I-6OT8qUMkmJ0KxgeCHz_l7D4_PaGSyjZEeaHdzKx0ou8ZyPUXFoqAm5yZq8BwJWNQlemoyq9VRExMDCciZ44f4uAWlTPOmS3gIYHNv5bEV5F9oQGAEAKZMS60fCi_Jl1WyuyZCgklrUjY4eMrS8uw2YHRB99Odt1LVo1BqC-2SnvsQ1rEAloW3nHYiLEhbeF8AclniwO8DM-Fl6vLNUcuFoeHAiBZm1xgh-3wsFAa2ezAVzMhD8U2jE2XPifapJciyH8fk8Oh01uIfckQTq0dJbeQrhSzpbHzw" // Замініть на ваш реальний токен
             language="en"
             config="parcelCollect"
-          ></inpost-geowidget>
+            onPoint={onPointCallback} // Callback при виборі поштомату
+          />
         </div>
         <input
           {...register("senderAddress.postamat", {
