85bf7cbc2665ec32791e22e7e44c52926dae5088
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-04-08 12:35:58 +0200

    update

--- src/services/sendToServer.js
+++ src/services/sendToServer.js
@@ -11,7 +11,7 @@ const sendShipmentData = async (state) => {
   const payment = stateData.value;
   const gabarytes = {
     A: { length: 64, width: 38, height: 8, weightActual: 4.86 },
-    B: { length: 64, width: 38, height: 19, weightActual: 11.55 },
+    B: { length: 64, width: 38, height: 19, weightActual: 10 },
     C: { length: 64, width: 38, height: 41, weightActual: 24.9 },
   };
 
